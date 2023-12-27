d4b48964068b1e03367dd8e47ac20b8b4b238256
maheshdvp <maheshdvp9@gmail.com>
2023-12-27 20:04:40 +0530

    roboshop-shell-ansible

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
-module "components" {
+module "vpc" {
   source = "git::https://github.com/Maheshdvp/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets =each.value["subnets"]
 
+}
+output "vpc" {
+  value = "module.vpc"
 }
