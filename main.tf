6ca46a759468631bf012a0c9fe3b7ea98fc3571d
maheshdvp <maheshdvp9@gmail.com>
2024-03-10 18:39:08 +0530

    roboshop-shell-ansible

--- main.tf
+++ main.tf
@@ -21,9 +21,8 @@ module "vpc" {
 #  lb_type=each.value["lb_type"]
 #  sg_ingress_cidr = each.value["sg_ingress_cidr "]
 #  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-#  tags = var.tags
-##  vpc_id = each.value["internal"] ? :var.default_vpc_id
 #  subnets = each.value["internal"] ?  : local.app_subnets
+#  tags = var.tags
 #  env =var.env
 #  sg_port =each.value["sg_port"]
 #}
